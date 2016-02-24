25c206fc031e6d56a2065482aa4ef3bb568dcc71
JesusMoran <jesus.mor.13@gmail.com>
2016-02-24 10:34:29 -0800

    Signed-off-by: JesusMoran <jesus.mor.13@gmail.com>

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Jesus E. Moran Rivera
+ *
+ * Created on February 23, 2016, 7:38 PM
+ */
+//system libraries
+
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//global constant
+
+//function prototype
+
+//executing begins here
+int main(int argc, char** argv) {
+    //declare variables
+    
+    //input values
+    
+    //map the input to the output
+    
+    //output the result
+    cout << "Hello World" << endl;
+    return 0;
+}
+
