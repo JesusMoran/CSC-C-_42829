da7bd39a99f764c78f0f1bd203fb77d640b00c52
JesusMoran <jesus.mor.13@gmail.com>
2016-02-25 22:18:46 -0800

    Signed-off-by: JesusMoran <jesus.mor.13@gmail.com>

---
+++ Homework-Savitch_Ch.1/Problem_5.cpp
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Jesus E Moran Rivera
+ *
+ * Created on February 23, 2016, 8:27 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( )
+ {
+    int width, hight, total_length;
+   
+    cout << "Hello.\n";
+    
+    cout << "Press return after entering a number:\n";
+    cout << "Enter the width of the fence:\n";
+    
+    cin >> width;
+    
+    cout << "Enter the hight of the fence:\n";
+    cin >> hight;
+    total_length = width * hight;
+    cout << "If you have a ";
+    cout << width;
+    cout << " feet wide fence\n";
+    cout << "and it's ";
+    cout << hight;
+    cout << " feet high, then \n";
+    cout << "you have ";
+    cout << total_length;
+    cout << " total feet of fence. \n";
+   
+    cout << "Good-bye.\n";
+    
+    return 0;
+}
+
