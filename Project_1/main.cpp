03d21f5159029b8819b1863e539eaccfa27ecf47
JesusMoran <jesus.mor.13@gmail.com>
2016-05-03 22:06:46 -0700

    Signed-off-by: JesusMoran <jesus.mor.13@gmail.com>

--- Project_1/main.cpp
+++ Project_1/main.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 //User Libraries
@@ -15,47 +17,44 @@ using namespace std;
 //Global Constants
 
 //Function Prototype
-void start(int);
-void liveCo(int);
+void start(unsigned short);
+void endGame(unsigned short, int);
 
 //Execution Begins Here
 
 int main(int argc, char** argv) {
     //Declare Variables
-    int lane;
+    unsigned short lane;
+    
+    ifstream in;
     
     cout<<"Use the number keys to move the car and press enter after each input. \n"
            "Use 1 to 4 and press any other key to exit."<<endl<<endl;
+    
     start(lane);
+            
     return 0;
 }
 /*******************************************************************************
 *******************************************************************************/
-void start(int lane)
+void start(unsigned short lane)
 {
     //Declare Variables
-    char grid[5][30]; 
-    int x, y, i, live=3; 
+    const int SIZE=5, LENGTH=30;
+    char grid[SIZE][LENGTH]; 
+    unsigned short x, y, i, live=3, score=0;
+    bool crash=false;
   
     //Generates initial map of 5 by 30
-        for (x=0;x<5;x++) 
-         {
-            for (y=0;y<30;y++) 
-            {
+        for (x=0;x<SIZE;x++){
+            for (y=0;y<LENGTH;y++){
                  grid[x][y]='_'; 
                  grid[x][y];
             }    
-         } 
-        for (x=0;x<5;x++) 
-         {
-            for (y=0;y<30;y++) 
-            {
-                 grid[x][y]; 
-            }
-    }
+        } 
     //Places "car" on the grid if it is above -1
     lane=0;
-    while((lane!=-1)&&(live>0)){
+    while(live>0){
         
         //Keeps track of lives for the player
         cout<<"Lives: "<<live<<endl;
@@ -67,37 +66,42 @@ void start(int lane)
         //Verifies that the inputs are in the lane range
         if(lane>=1&&lane<=4){
             
+            //If player hits a block they lose a live
+            if(grid[lane][28]==']'){ 
+               crash=true;
+               live-=1;
+            }
+            else{
+                 score+=10;
+            }
+            
             //Random lane selection for Obstacles
-            srand(time(NULL));
+            srand(static_cast<unsigned int>(time(0)));
             i=rand()%5+1;
 
             //Places obstacle once a lane is picked
             grid[i][0]=']';
-            for(x=0;x<5;x++){
+            
+            for(x=0;x<SIZE;x++){
              grid[x][29]='_';  
             }
-            
             //Places car in the selected lane
             grid[lane][29]='<';
-
+            
             //Places obstacle and moves it across the grid once a new one spawns
-            for (x=0;x<5;x++) 
-             {
-                for (y=0;y<30;y++) 
-                {
+            for (x=0;x<SIZE;x++){
+                for (y=0;y<LENGTH;y++){
                    if(y<29){
                        if(grid[x][y]==']'){
                            grid[x][y+1]=grid[x][y];
                            grid[x][y]='_';
                            y++;
-                       }
-                   }
+                        }
+                    }
                 }
             }
-            for (x=0;x<5;x++) 
-             {
-                for (y=0;y<30;y++) 
-                {
+            for (x=0;x<SIZE;x++){
+                for (y=0;y<LENGTH;y++){
                  cout<<grid[x][y]; 
                 }
                 cout<<endl;
@@ -106,10 +110,33 @@ void start(int lane)
         }else
               cout<<"Stay in the lane!!!"<<endl;
     }
+    if (live<1)
+        endGame(live, score);
 }
 /*******************************************************************************
 *******************************************************************************/
-void liveCo(int live)
+void endGame(unsigned short live, int score)
 {
+    const short PLACE=10;
+    char name[PLACE];
     
+        cout<<"Game Over!!!"<<endl<<endl;
+        
+        cout<<"Enter your name: (max of 10 characters)\n";
+        cin>>name;
+        
+        cout<<endl;
+        
+        cout<<"Name        Score"<<endl;
+        cout<<name<<setw(13)<<score<<endl;   
+        
+        ofstream out;
+        
+        //Output the results to a file
+        out.open("LA_Traffic_Score.dat",ios::app);
+        out<<"\r\n"<<name<<setw(13)<<score<<endl;
+        
+        out.close();
 }
+/*******************************************************************************
+*******************************************************************************/
